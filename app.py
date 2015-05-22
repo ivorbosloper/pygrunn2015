e514d5495f68b06997ffc05f974dc603fb4fafb7
Ivor Bosloper <ivorbosloper@gmail.com>
2015-05-22 09:41:32 +0200

    Forget this import

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from pygrunn.pygrunn1 import *
 from pygrunn.pygrunn2 import *
 from pygrunn.pygrunn3 import *
 from pygrunn.pygrunn5 import *
-from pygrunn.pygrunn6 import *
 
 if __name__ == '__main__':
     app.run(debug=True)
